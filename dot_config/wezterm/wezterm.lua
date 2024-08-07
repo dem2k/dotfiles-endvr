0d431ae52bf4ab832c00a2f6443ca45bc26d20bf
User Name <user@server.com>
2024-08-07 21:44:52 +0200

    chages xtest user

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -12,6 +12,18 @@ config.enable_tab_bar = false
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 14
 config.color_scheme = "Tokyo Night"
--- and finally, return the configuration to wezterm
+
+config.window_frame = {
+  border_left_width = '0.2cell',
+  border_right_width = '0.2cell',
+  border_bottom_height = '0.1cell',
+  border_top_height = '0.1cell',
+  border_left_color = 'silver',
+  border_right_color = 'silver',
+  border_bottom_color = 'silver',
+  border_top_color = 'silver',
+}
+
+-- and fnally, return the configuration to wezterm
 return config
 
