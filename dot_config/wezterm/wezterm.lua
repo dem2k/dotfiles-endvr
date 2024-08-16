2190a79e03078085da033d116d7336582d2c2683
User Name <user@server.com>
2024-08-17 00:34:40 +0200

    chages

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,24 +5,24 @@ local config = wezterm.config_builder()
 --
 -- config.default_prog = {"/usr/bin/zsh"}
 --
-config.initial_cols = 160
-config.initial_rows = 48
+config.initial_cols = 150
+config.initial_rows = 40
 config.enable_tab_bar = false
 --
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 14
 config.color_scheme = "Tokyo Night"
 
-config.window_frame = {
-  border_left_width = '0.2cell',
-  border_right_width = '0.2cell',
-  border_bottom_height = '0.1cell',
-  border_top_height = '0.1cell',
-  border_left_color = 'silver',
-  border_right_color = 'silver',
-  border_bottom_color = 'silver',
-  border_top_color = 'silver',
-}
+-- config.window_frame = {
+--   border_left_width = '0.2cell',
+--   border_right_width = '0.2cell',
+--   border_bottom_height = '0.1cell',
+--   border_top_height = '0.1cell',
+--   border_left_color = 'silver',
+--   border_right_color = 'silver',
+--   border_bottom_color = 'silver',
+--   border_top_color = 'silver',
+-- }
 
 -- and fnally, return the configuration to wezterm
 return config
