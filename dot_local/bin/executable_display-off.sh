2472f8fb004eef4d0fe4f8168e497d2c07da3c53
dk <dk@eqr6>
2025-10-10 21:47:00 +0200

    Add .local/bin/ddd Add .local/bin/display-off.sh Add .local/bin/display-on.sh

---
+++ dot_local/bin/executable_display-off.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wlr-randr --output HDMI-A-1 --off
+
+echo '/home/dk/bin/display-on.sh'|at 08:01 
+
---
+++ dot_local/bin/executable_display-on.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wlr-randr --output HDMI-A-1 --on
+
+echo '/home/dk/bin/display-off.sh'|at 23:05
+
