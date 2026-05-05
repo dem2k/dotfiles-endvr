767542a54c0326fe3fe5a03f6c2cf405fe32b803
dk <dk@eqr6>
2026-05-06 00:55:40 +0200

    Update .config/nvim/lua/plugins/mason-lsp-config.lua

--- dot_config/nvim/lua/plugins/mason-lsp-config.lua
+++ dot_config/nvim/lua/plugins/mason-lsp-config.lua
@@ -6,25 +6,20 @@ return {
         { "williamboman/mason.nvim",           opts = {} },
         { "williamboman/mason-lspconfig.nvim", opts = {} },
         { "j-hui/fidget.nvim",                 opts = {} }, -- Useful status updates for LSP
-        { "folke/neodev.nvim",                 opts = {} }, -- Additional lua configuration, makes nvim stuff amazing!
+        -- neodev entfernt: ist für nvim 0.11+ nicht mehr nötig
     },
     config = function()
         require("mason").setup()
         require("mason-lspconfig").setup()
 
-        local lspconfig = require("lspconfig")
-        local lspdefault = lspconfig.util.default_config
-
-        lspdefault.capabilities = vim.tbl_deep_extend(
-            "force", lspdefault.capabilities,
-            require("cmp_nvim_lsp").default_capabilities()
-        )
+        -- Globale Capabilities (ersetzt lspconfig.util.default_config)
+        vim.lsp.config('*', {
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        })
 
         vim.api.nvim_create_autocmd("LspAttach", {
-            -- group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
             desc = "LSP Actions",
             callback = function(event)
-                -- vim.lsp.set_log_level("debug")
                 local keymap = function(keys, func, desc)
                     vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
                 end
@@ -49,38 +44,27 @@ return {
 
                 keymap('<leader>lx1', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', "add wsp fldr")
                 keymap('<leader>lx2', ':lua vim.lsp.buf.remove_workspace_folder()<CR>', "rm wsp fldr")
-                keymap('<leader>lx3', ':lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-                    "list wsp fldr")
+                keymap('<leader>lx3', ':lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', "list wsp fldr")
                 keymap('<leader>lx5', ':lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', "show line diagn")
             end
         })
 
-        lspconfig.lua_ls.setup({
+        -- lua_ls
+        vim.lsp.config('lua_ls', {
             settings = {
                 Lua = {
-                    completion = {
-                        callSnippet = "Replace"
-                    },
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                        version = "LuaJIT",
-                    },
-                    diagnostics = {
-                        -- Get the language server to recognize the `vim` global
-                        globals = { "vim", },
-                    },
+                    completion = { callSnippet = "Replace" },
+                    runtime = { version = "LuaJIT" },
+                    diagnostics = { globals = { "vim" } },
                     workspace = {
-                        -- Make the server aware of Neovim runtime files
                         checkThirdParty = false,
                         library = vim.api.nvim_get_runtime_file("", false),
                     },
-                    -- Do not send telemetry data containing a randomized but unique identifier
-                    telemetry = {
-                        enable = false,
-                    },
+                    telemetry = { enable = false },
                 }
             }
         })
+        vim.lsp.enable('lua_ls')
 
 --         -- powershell
 --         local pwsh_home = vim.env.SCOOP or "/usr/lib/pwsh/default"
@@ -116,17 +100,15 @@ return {
 --             end
 --         }) -- end powershell
 
-        -- java, last version of jdtls supports java 11 should be 1.12.0. how to download and install this particular verstion with mason? goddamn!
-        local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-        local java_home = vim.env.JAVA_HOME or "/usr/lib/jvm/default"
-        local jdtls_home = vim.env.JDTLS_HOME or (vim.fn.stdpath("data") .. "/mason/packages/jdtls")
-        local jdtls_config = jdtls_home .. "/config_win"
-        local jdtls_launcher = jdtls_home .. "/plugins/org.eclipse.equinox.launcher_*.jar"
-        local data_tmp = vim.env.TEMP or "/tmp"
 
-        lspconfig.jdtls.setup {
+        -- jdtls (java)
+        local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+        local java_home    = vim.env.JAVA_HOME or "/usr/lib/jvm/default"
+        local jdtls_home   = vim.env.JDTLS_HOME or (vim.fn.stdpath("data") .. "/mason/packages/jdtls")
+        local data_tmp     = vim.env.TEMP or "/tmp"
+
+        vim.lsp.config('jdtls', {
             cmd = {
-                --
                 java_home .. "/bin/java",
                 "-Declipse.application=org.eclipse.jdt.ls.core.id1",
                 "-Dosgi.bundles.defaultStartLevel=4",
@@ -143,13 +125,10 @@ return {
                 "--add-modules=ALL-SYSTEM",
                 "--add-opens", "java.base/java.lang=ALL-UNNAMED",
                 "--add-opens", "java.base/java.util=ALL-UNNAMED",
-                --
                 "-javaagent:" .. jdtls_home .. "/lombok.jar",
-                --
-                "-jar", vim.fn.glob(jdtls_launcher),
-                "-configuration", jdtls_config,
-                -- "-data", (vim.fs.dirname(vim.fs.find({"pom.xml"}, { upward = true })[1]) or vim.fn.getcwd()) .. "/.jdtls"
-                "-data", data_tmp .. "/.jdtls-" .. project_name
+                "-jar", vim.fn.glob(jdtls_home .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+                "-configuration", jdtls_home .. "/config_win",
+                "-data", data_tmp .. "/.jdtls-" .. project_name,
             },
             settings = {
                 java = {
@@ -157,32 +136,13 @@ return {
                     signatureHelp = { enabled = true },
                     import = { enabled = true },
                     rename = { enabled = true },
-                    eclipse = { downloadSources = true, },
-                    maven = { downloadSources = true, },
+                    eclipse = { downloadSources = true },
+                    maven = { downloadSources = true },
                 }
             },
-            init_options = {
-                bundles = {}
-            },
-
-            -- root_dir = function()
-            --   return {
-            --         "pom.xml", -- Maven
-            --         "build.xml", -- Ant
-            --       }          -- Multi-module projects { "build.gradle", "build.gradle.kts" },
-            --       -- or vim.fn.getcwd()
-            -- end,
-
+            init_options = { bundles = {} },
             single_file_support = true,
-        }
-        -- lspconfig.jdtls.setup().find_root({ "pom.xml" })
-        -- require("jdtls.setup").find_root({ "pom.xml" })
-
-        -- local cmp = require("cmp")
-        -- cmp.setup({
-        --   mapping = {
-        --     ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        --   }
-        -- })
+        })
+        vim.lsp.enable('jdtls')
     end
 }
